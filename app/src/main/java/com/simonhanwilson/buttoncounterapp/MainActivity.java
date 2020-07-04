7ac5dd851d71a2ce2f74ddc132334406646854a2
simonw101a <simonw101@gmail.com>
2020-07-04 16:47:39 +0100

    lecture 38

--- app/src/main/java/com/simonhanwilson/buttoncounterapp/MainActivity.java
+++ app/src/main/java/com/simonhanwilson/buttoncounterapp/MainActivity.java
@@ -3,12 +3,31 @@ package com.simonhanwilson.buttoncounterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userInput;
+    private Button button;
+    private TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        textview = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
